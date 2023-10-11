2afb76a0119e5f8c2ddee269492bc0bb488d031b
VadimKorobec <_s_e_t__@ukr.net>
2023-10-11 17:36:15 +0300

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,25 +1,14 @@
 import { useDispatch } from "react-redux";
-
 import { MdClose } from "react-icons/md";
-import { BsPencil } from "react-icons/bs";
-import { deleteTask, changeTask, toggleCompleted } from "../../redux/actions";
-
+import { deleteTask, toggleCompleted } from "../../redux/tasksSlice";
 import css from "./Task.module.css";
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
 
-  const handleDelete = () => {
-    dispatch(deleteTask(task.id));
-  };
+  const handleDelete = () => dispatch(deleteTask(task.id));
 
-  const handleChange = () => {
-    dispatch(changeTask(task.id));
-  };
-
-  const handleToggle = () => {
-    dispatch(toggleCompleted(task.id));
-  };
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
 
   return (
     <div className={css.wrapper}>
@@ -30,10 +19,7 @@ export const Task = ({ task }) => {
         onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
-      <button onClick={handleChange} className={css.btn}>
-        <BsPencil size={20} />
-      </button>
-      <button onClick={handleDelete} className={css.btn}>
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
