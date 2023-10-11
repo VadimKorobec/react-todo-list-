69f3bf777d3dc2b9bc3e6cb932cd2f84bc5dc553
VadimKorobec <_s_e_t__@ukr.net>
2023-10-11 16:07:16 +0300

    Update actions.js

---
+++ src/redux/actions.js
@@ -0,0 +1,40 @@
+import { nanoid } from "nanoid";
+
+export const addTask = (text) => {
+  return {
+    type: "tasks/addTask",
+    payload: {
+      id: nanoid(),
+      completed: false,
+      text,
+    },
+  };
+};
+
+export const changeTask = (taskId) =>{
+  return{
+    type:'tasks/changeTask',
+    payload:taskId,
+  }
+}
+
+export const deleteTask = (taskId) => {
+  return {
+    type: "tasks/deleteTask",
+    payload: taskId,
+  };
+};
+
+export const toggleCompleted = (taskId) => {
+  return {
+    type: "tasks/toggleCompleted",
+    payload: taskId,
+  };
+};
+
+export const setStatusFilter = (value) => {
+  return {
+    type: "filters/setStatusFilter",
+    payload: value,
+  };
+};
