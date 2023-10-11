db37b04bb99c07c156c2ed714abb4d6356cca05b
VadimKorobec <_s_e_t__@ukr.net>
2023-10-11 18:11:16 +0300

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Task } from "../Task/Task";
 import { getTasks, getStatusFilter } from "../../redux/selectors";
-import { statusFilters } from "../../redux/constants";
+import { statusFilters } from '../../redux/constants'
 import css from "./TaskList.module.css";
 
 const getVisibleTasks = (tasks, statusFilter) => {
