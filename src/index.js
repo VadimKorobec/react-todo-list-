d6837632a49e89753f72778c0081ceaab0419f47
VadimKorobec <_s_e_t__@ukr.net>
2023-10-11 19:37:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { App } from "./components/App";
 import { store } from "./redux/store";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
