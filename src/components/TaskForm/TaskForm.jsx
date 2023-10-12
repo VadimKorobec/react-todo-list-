c26e336daad3609cf394458d9d3714807f18bff0
VadimKorobec <_s_e_t__@ukr.net>
2023-10-12 13:55:40 +0300

    Update TaskForm.jsx

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,45 +1,56 @@
 import { useDispatch } from "react-redux";
-import { Formik,Form,Field } from 'formik';
-import Button from 'react-bootstrap/Button';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import Button from "react-bootstrap/Button";
 import { addTask } from "../../redux/tasksSlice";
 import css from "./TaskForm.module.css";
 
-const initialValues ={
-  title:'',
-  text:'',
-}
+let schema = yup.object().shape({
+  title: yup.string().required(),
+  text: yup.string().required(),
+});
+
+const initialValues = {
+  title: "",
+  text: "",
+};
 
 export const TaskForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (values,{resetForm}) => {
-   console.log(values)
-   
-   resetForm()
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+
+    resetForm();
   };
 
   return (
     <>
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-    <Form className={css.form} >
-      <Field
-        className={css.field}
-        type="text"
-        name="title"
-        placeholder="Enter task title..."
-      />
-      <Field
-        className={css.field}
-        type="text"
-        name="text"
-        placeholder="Enter task text..."
-      />
-      <Button variant="primary" type="submit">
-        create Task
-      </Button>
-    </Form>
-    </Formik>
-    
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.form}>
+          <Field
+            className={css.field}
+            type="text"
+            name="title"
+            placeholder="Enter task title..."
+          />
+          <ErrorMessage name="title" component="div" />
+          <Field
+            className={css.field}
+            type="text"
+            name="text"
+            placeholder="Enter task text..."
+          />
+          <ErrorMessage name="text" component="div" />
+          <Button variant="primary" type="submit">
+            create Task
+          </Button>
+        </Form>
+      </Formik>
     </>
   );
 };
